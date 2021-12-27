29b205f4d553b315319b12b83334161ef842375c
inzimam Ul Haq <inzemamsmile2@gmail.com>
2021-12-27 23:10:54 +0500

    using react hooks

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {server} from '../../lib/api'
+import {DeleteListingsData, DeleteListingsVariables,ListingsData, Listing} from "./types";
 
 const LISTINGS = `query Listings {
   listings {
@@ -14,16 +15,50 @@ const LISTINGS = `query Listings {
   }
 }`;
 
+const DELETE_LISTING = `mutation DeleteListing($id: ID!) {
+  deleteListing(id: $id) {
+    id
+    title
+    image
+    address
+    price
+    numOfBeds
+    numOfBaths
+    numOfGuests
+    rating
+  }
+}`;
+
 interface Props {
     title: string
 }
 export const Listings = ({ title }: Props) => {
+    const [ listings, setListings ] = useState<Listing[] | null>(null);
+
+    useEffect(( ) => {
+        fetchListing()
+            .catch(reason => console.error(`fetch listing failed: ${reason}`));
+    }, [])
     const fetchListing = async () => {
-        const {data} = await server.fetch({ query: LISTINGS })
+        const {data} = await server.fetch<ListingsData>({ query: LISTINGS })
         console.log(data)
+        setListings(data.listings);
     }
+    const listingsList = listings?.map(listing =>
+        <li key={listing.id}>
+            {listing.title}
+            <button onClick={() => deleteListing(listing.id)}>Delete</button>
+        </li>
+    )
     return <div>
         <h2>{title}</h2>
-        <button onClick={fetchListing}>Query Listings!</button>
+        {listingsList}
     </div>
+}
+
+const deleteListing = async (id: string) => {
+    await server.fetch<DeleteListingsData, DeleteListingsVariables>({
+        query: DELETE_LISTING,
+        variables: {id}
+    })
 }
